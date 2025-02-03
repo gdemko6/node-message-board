356cf06d4b5008503615e2d1741a6a05f0d07d38
gdemko6 <gregory.demko@my.uwrf.edu>
2025-02-03 15:29:37 -0600

    Add hard coded message objects to an array for test purposes

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 
+const messages = [
+  { text: "Hello", user: "Greg", added: new Date() },
+  {
+    text: "yo",
+    user: "Charles",
+    added: new Date(),
+  },
+  { text: "whats up", user: "Bob", added: new Date() },
+];
+
 app.get("/", (req, res) => {
-    res.send("Hello, world");
-})
+  res.send("Hello, world");
+});
 
 app.listen(3000, () => {
-    console.log("node app");
-})
+  console.log("node app");
+});

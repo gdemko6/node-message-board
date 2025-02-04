60b680fb52bea9412063a78563dd9b0c6bcd0a21
gdemko6 <gregory.demko@my.uwrf.edu>
2025-02-04 14:56:44 -0600

    test

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const messages = [
   { text: "Hello", user: "Greg", added: new Date() },

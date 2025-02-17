4f1c9f709a88f22fadc5def531d837203d5d30e6
gdemko6 <gregory.demko@my.uwrf.edu>
2025-02-17 11:17:34 -0600

    Allow user to create new message in /new

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const messages = [
   { text: "Hello", user: "Greg", added: new Date() },
   {
@@ -19,6 +21,14 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { messages: messages });
 });
 
-app.listen(3100, () => {
-  console.log("node app");
+app.get("/new", (req, res) => {
+  res.render("form.ejs");
 });
+
+app.post("/new", (req, res) => {
+  message = req.body;
+  messages.push({ text: message.text, user: message.user, added: new Date() });
+  res.redirect("/");
+});
+
+app.listen(3100, () => {});
